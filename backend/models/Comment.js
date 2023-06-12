6543603dc27da5ae1c2c6d140fe2a48531d05f4f
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-06-12 17:26:54 -0400

    adding likes to comments and posts

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -2,20 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  post: {
-    type: Schema.Types.ObjectId,
-    ref: 'Post'
-  },
-  text: {
-    type: String,
-    required: true
-  }
-}, {
-  timestamps: true
-});
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: 'Post'
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    likes: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }]
+  }, {
+    timestamps: true
+  });
 
 module.exports = mongoose.model('Comment', commentSchema);
