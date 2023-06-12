d57fae62c657b6e7042b84cf6bee429abc04c9b3
Merve Dogan-Espaillat <1mervedogan@gmail.com>
2023-06-12 17:05:02 -0400

    style index page

--- frontend/src/components/Posts/PostBox.js
+++ frontend/src/components/Posts/PostBox.js
@@ -1,13 +1,35 @@
 import "./PostBox.css"
+import image from "./profile.png"
 
 function PostBox ({ post: { text, author }}) {
   const { username } = author;
   return (
-    <div className="post">
-      <h3>{username}</h3>
-      <p>{text}</p>
+    <div className="post-con">
+<div className="post-image">
+  <div className="artist">
+    <h1>ART</h1>
+  </div>
+  <div className="artist-img"></div>
+</div>
+<div className="post-det">
+<div className="post-username">
+  <div className="username"></div>
+  <div className="artist-name"><h1>ARTIST</h1></div>
+</div>
+<div className="post-like">
+  <div className="user-image">
+    <img src={image} alt="" />
+  </div>
+  <div className="user-username"><h3>{username}</h3></div>
+
+</div>
+</div>
+<div className="post-description">
+<p>{text}</p>
+</div>
     </div>
   );
 }
 
 export default PostBox;
+
