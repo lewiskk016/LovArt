6cfb4142eac4fb1adc0f931793203e14769e839f
Merve Dogan-Espaillat <1mervedogan@gmail.com>
2023-06-12 10:41:27 -0400

    setup frontend auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Switch } from "react-router-dom";
+
+import { AuthRoute, ProtectedRoute } from "./components/Routes/Routes";
+import NavBar from "./components/NavBar/NavBar";
+
+import MainPage from "./MainPage/MainPage";
+
+import LoginForm from "./components/SessionForms/LoginForm";
+import SignupForm from "./components/SessionForms/SignupForm";
+import Posts from "./components/Posts/Posts";
+import Profile from "./components/Profile/Profile";
+import PostCompose from "./components/Posts/PostCompose";
+
+import { getCurrentUser } from "./store/session";
+
 function App() {
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCurrentUser()).then(() => setLoaded(true));
+  }, [dispatch]);
+
   return (
-    <h1>Hello from App</h1>
+    loaded && (
+      <>
+        <NavBar />
+        <Switch>
+          <AuthRoute exact path="/" component={MainPage} />
+          <AuthRoute exact path="/login" component={LoginForm} />
+          <AuthRoute exact path="/signup" component={SignupForm} />
+
+          <ProtectedRoute exact path="/Posts" component={Posts} />
+          <ProtectedRoute exact path="/profile" component={Profile} />
+          <ProtectedRoute exact path="/Posts/new" component={PostCompose} />
+        </Switch>
+      </>
+    )
   );
 }
 
-export default App;
+export default App
