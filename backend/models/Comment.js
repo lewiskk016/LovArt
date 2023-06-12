f46827fcfd656776ebb5022ba553ed8b733e742a
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-06-12 17:00:51 -0400

    added comments in the backend

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,23 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
-const postSchema = new Schema({
+const commentSchema = new Schema({
   author: {
     type: Schema.Types.ObjectId,
     ref: 'User'
   },
+  post: {
+    type: Schema.Types.ObjectId,
+    ref: 'Post'
+  },
   text: {
     type: String,
     required: true
-  },
-  comments: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Comment'
-  }]
+  }
 }, {
   timestamps: true
 });
 
-
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model('Comment', commentSchema);
---
+++ backend/routes/api/comments.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const mongoose = require('mongoose');
+const Post = mongoose.model('Post');
+const Comment = mongoose.model('Comment');
+const { requireUser } = require('../../config/passport');
+const validateCommentInput = require('../../validations/posts.js');
+
+// GET all comments for a specific post
+router.get('/', async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ post: req.params.postId })
+      .populate('author', '_id username')
+      .sort({ createdAt: -1 });
+
+    return res.json(comments);
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+// POST create a new comment for a specific post
+router.post('/', requireUser, validateCommentInput, async (req, res, next) => {
+    try {
+      const post = await Post.findOne({_id: req.params.postId});
+      if (!post) {
+        const error = new Error('Post not found');
+        error.statusCode = 404;
+        error.errors = { message: 'No post found with that id' };
+        return next(error);
+      }
+  
+      const newComment = new Comment({
+        text: req.body.text,
+        author: req.user._id,
+        post: post._id,
+      });
+  
+      let comment = await newComment.save();
+      comment = await Comment.populate(comment, {path: 'author', select: '_id username'});
+  
+      // Add the new comment to the post's comments array
+      post.comments.push(comment._id);
+      await post.save();
+  
+      return res.json(comment);
+    } catch (err) {
+      next(err);
+    }
+  });
+  
+  
+
+// GET a specific comment for a specific post
+router.get('/:commentId', async (req, res, next) => {
+  try {
+    const comment = await Comment.findOne({
+      _id: req.params.commentId,
+      post: req.params.postId,
+    }).populate('author', '_id username');
+
+    if (!comment) {
+      const error = new Error('Comment not found');
+      error.statusCode = 404;
+      error.errors = { message: 'No comment found with that id' };
+      return next(error);
+    }
+
+    return res.json(comment);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// PATCH update a specific comment for a specific post
+router.patch('/:commentId', requireUser, validateCommentInput, async (req, res, next) => {
+  try {
+    const comment = await Comment.findOne({
+      _id: req.params.commentId,
+      post: req.params.postId,
+    });
+
+    if (!comment) {
+      const error = new Error('Comment not found');
+      error.statusCode = 404;
+      error.errors = { message: 'No comment found with that id' };
+      return next(error);
+    }
+
+    // Check if the authenticated user is the author of the comment
+    if (comment.author.toString() !== req.user._id.toString()) {
+      const error = new Error('Unauthorized');
+      error.statusCode = 401;
+      error.errors = { message: 'You are not authorized to edit this comment' };
+      return next(error);
+    }
+
+    // Update the comment with the new text
+    comment.text = req.body.text;
+    const updatedComment = await comment.save();
+
+    return res.json(updatedComment);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// DELETE a specific comment for a specific post
+router.delete('/:commentId', requireUser, async (req, res, next) => {
+  try {
+    const comment = await Comment.findOne({
+      _id: req.params.commentId,
+      post: req.params.postId,
+    });
+
+    if (!comment) {
+      const error = new Error('Comment not found');
+      error.statusCode = 404;
+      error.errors = { message: 'No comment found with that id' };
+      return next(error);
+    }
+
+    // Check if the authenticated user is the author of the comment
+    if (comment.author.toString() !== req.user._id.toString()) {
+      const error = new Error('Unauthorized');
+      error.statusCode = 401;
+      error.errors = { message: 'You are not authorized to delete this comment' };
+      return next(error);
+    }
+
+    await Comment.deleteOne({ _id: comment._id });
+
+    return res.json({ success: true });
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
