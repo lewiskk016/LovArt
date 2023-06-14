842e548e895c05661ccba0059cf98277c6c67bc7
Merve Dogan-Espaillat <1mervedogan@gmail.com>
2023-06-14 15:00:45 -0400

    carousel setup

--- frontend/src/components/Carousel/Carousel.js
+++ frontend/src/components/Carousel/Carousel.js
@@ -1,30 +1,65 @@
-import { useEffect, useState } from "react"
-import './Carousel.css'
+import { useEffect, useState } from "react";
+import "./Carousel.css";
 
-const Carousel = () =>{
+const Carousel = () => {
+  const data = [
+    `"To practice any art, no matter how well or badly, is a way to make your soul grow. So do it."`,
+    `"Every artist dips his brush in his own soul, and paints his own nature into his pictures."`,
+    `"Painting is poetry that is seen rather than felt, and poetry is painting that is felt rather than seen."`,
+  ];
 
-    const data = ["To practice any art, no matter how well or badly, is a way to make your soul grow. So do it.", "Every artist dips his brush in his own soul, and paints his own nature into his pictures.","Painting is poetry that is seen rather than felt, and poetry is painting that is felt rather than seen."]
-    const [currentIndex, setCurrentIndex] = useState(0)
-    const carouselInfiniteScroll = () => {
-        if (currentIndex === data.length-1){
-            return setCurrentIndex(0)
-        }
-        return setCurrentIndex(currentIndex + 1)
+  const data1 = [
+    "- Kurt Vonnegut ",
+    "- Henry Ward Beecher",
+    "- Leonardo da Vinci",
+  ];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const carouselInfiniteScroll = () => {
+    if (currentIndex === data.length - 1 && currentIndex === data1.length - 1) {
+      return setCurrentIndex(0);
     }
-    useEffect( ()=> {
-        const interval =  setInterval(()=> {carouselInfiniteScroll()},3000)
-        return () => clearInterval(interval)
-    })
+    return setCurrentIndex(currentIndex + 1);
+  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      carouselInfiniteScroll();
+    }, 5000);
+    return () => clearInterval(interval);
+  });
 
-    return (
-        <div className="carousel-container">
-            {data.map((item, index)=> {
-                return <h1 className="carousel-item" 
-                style={{transform: `translate(-${currentIndex * 100}%)`}}
-                key={index}>{item} 
-                </h1>
-            })}
-        </div>
-    )
-}
-export default Carousel
+  return (
+    <>
+      <div className="carousel-container">
+        {data.map((item, index) => {
+          return (
+            <>
+              <p
+                className="carousel-item"
+                style={{ transform: `translate(-${currentIndex * 100}%)` }}
+                key={index}
+              >
+                {item}
+              </p>
+            </>
+          );
+        })}
+      </div>
+      <div className="carousel-container">
+        {data1.map((item, index) => {
+          return (
+            <>
+              <h1
+                className="carousel-item"
+                style={{ transform: `translate(-${currentIndex * 100}%)` }}
+                key={index}
+              >
+                {item}
+              </h1>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+export default Carousel;
